0516c24876191ad07e1423589e680f9223fafd75
guigui64 <guillaume.comte10@gmail.com>
2023-04-04 12:14:18 +0200

    nvim gopls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -24,6 +24,7 @@ return {
       denols = {
         root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
       },
+      gopls = {},
       jdtls = {
         root_dir = require("lspconfig").util.root_pattern(".project"),
         settings = {
